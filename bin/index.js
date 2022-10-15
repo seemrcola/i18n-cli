fb624b38e8a7a462e1582fc30c82f5013d5c809c
seemrcola <seemrcola@163.com>
2022-10-15 11:45:36 +0800

    init

---
+++ bin/index.js
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+import { program } from 'commander'
+import { PKG } from '../constant/index.js'
+
+/* add description */
+program
+  .option('-d --description', 'a cli for i18n')
+
+program
+  .command('export')
+  .description('export Chinese')
+  .action(() => {
+    import('../lib/export/index.js')
+  })
+
+program
+.command('translate <to>')
+.description('translate Chinese to English')
+.action(() => {
+    import('../lib/translate/index.js')
+})
+
+program
+.command('replace')
+.description('replace English to Chinese')
+.action(() => {
+    return console.log('功能仅为测试时的功能，不建议使用')
+    import('../lib/replace/index.js')
+})
+
+/* add version */
+program.version(PKG.version)
+
+/* parse */
+program.parse()
---
+++ constant/index.js
@@ -0,0 +1,20 @@
+import { cwd } from 'process'
+import fs from 'fs'
+import path from 'path'
+
+export const ROOT = cwd()
+
+export const PKG = JSON.parse(
+  fs.readFileSync(path.join(ROOT, 'package.json'), { encoding: 'utf-8' })
+)
+
+export const CTX = PKG.ctx
+
+export const FILESTYPE = ['js', 'ts', 'vue']
+
+export const PATH = 'src'
+
+export const LANG = 'en'
+
+export const EXPORT_JSON_NAME = 'c2x.index.json'
+
